9e4137b7ce881722282fd5e6c7e1528a50f577bc
Matthew Cole <colematthewg@gmail.com>
2019-02-17 16:11:16 -0600

    Travis

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// set my starter values
+var randoNum = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var loses = 0;
+var guesses = 9;
+var yoGuesses = [];
+
+
+for (var i = 0; i < 9; i++) {
+    console.log(i + 1);
+}
+// document.onkeyup = function(event) {
+
+// }
+// generate a random letter
+// prompt user to have them guess the letter
+// check to see if it is correct
+// check to see if letter is correct
+// if letter is wrong, allow user to try again. Repeat until they win, or are out of attempts
+// start over once someone wins or loses
